05ac71865f78c240c0d7c23a8b5039c317e01e5c
sjakedude <sjakedude@gmail.com>
2021-08-03 19:46:34 -0400

    lets goooooo

--- q1.py
+++ q1.py
@@ -2,15 +2,17 @@
 Jake Stephens
 Class: CS 677 - Spring 2
 Date: 8/3/2021
-Homework #4 - Q1
+Homework #5
 Analyzing patient data.
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import numpy as np
 import seaborn as sn
-
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import LabelEncoder
 
 
 # Adding Color column based on class
@@ -23,11 +25,10 @@ def calculate_fetal_state(row):
 
 def main():
 
-
     # ==================
     # Question #1
     # ==================
-    
+
     # Reading in dataframe
     df = pd.ExcelFile("data/CTG.xls").parse(2).drop(0).reset_index()
 
@@ -36,6 +37,24 @@ def main():
 
     print(df["fetal state"])
     print("hello world")
+
+    # ==================
+    # Question #2
+    # ==================
+
+    # Separating into x and y
+    x = df[["LB", "MLTV", "Width", "Variance"]]
+    y = df["fetal state"]
+
+    print(x)
+
+    # Splitting 50:50
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.5, random_state=1, shuffle=True
+    )
+
+    NB_classifier = MultinomialNB().fit(x_train, y_train)
+    y_predict = NB_classifier.predict(x_test)
 
 
 main()
